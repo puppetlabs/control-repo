368cb4adf0bffcae52842a2d73ff22a38136bf04
Nick Walker <npwalker@users.noreply.github.com>
2015-05-15 11:37:47 -0700

    Update configure_r10k.pp for PE3.8

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -2,16 +2,15 @@
 ##  Configure R10k ##
 ######         ######
 
-##  This manifest requires the zack/R10k module and will attempt to
-##  configure R10k according to my blog post on directory environments.
+##  This manifest requires the zack/R10k module
 ##  Beware! (and good luck!)
 
 class { 'r10k':
-  version           => '1.4.1',
+  version           => '1.5.1',
   sources           => {
     'puppet' => {
       #Edit remote to be your own control_repo
-      'remote'  => 'https://github.com/npwalker/control_repo.git',
+      'remote'  => 'https://github.com/npwalker/control-repo.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
     }

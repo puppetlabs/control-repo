5d0ae0c01fc5f3dbc1da07fe0a58ea1b36f2ead6
Nick Walker <nick.walker@puppetlabs.com>
2015-08-03 17:45:12 -0700

    fix config_version script for 3.8 and below

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -3,5 +3,5 @@ if [ -e /opt/puppetlabs/server/pe_version ]
 then 
   /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2  
 else
-  /usr/bin/git --git-dir $codedir/environments/$environment/.git rev-parse HEAD
+  /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD
 fi

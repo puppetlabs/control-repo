6a78949ca8b24fd0e655b34cfc1406d967fe6e26
Nick Walker <nick.walker@puppetlabs.com>
2015-08-13 17:10:55 -0700

    Fix some incorrect quoting in the hiera class
    I thought I needed to double quote items that had interpolated
    variables but it turns out I don't need to which is good
    because I effectively can't due to .to_yaml not doing what I
    wanted it to do.

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -2,11 +2,11 @@ class profile::puppetmaster {
 
   class { 'hiera':
     hierarchy  => [
-      "\"nodes/%{::trusted.certname}\"",
-      "common",
+      'nodes/%{::trusted.certname}',
+      'common',
     ],
     hiera_yaml => '/etc/puppetlabs/code/hiera.yaml',
-    datadir    => "\"/etc/puppetlabs/code/environments/%{environment}/hieradata\"",
+    datadir    => '/etc/puppetlabs/code/environments/%{environment}/hieradata\',
     owner      => 'root',
     group      => 'root',
   }

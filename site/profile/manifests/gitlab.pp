184cf7e80e1c1585e6365181e8b6528bcee32977
Nick Walker <nick.walker@puppetlabs.com>
2015-08-17 15:45:22 -0700

    Change default gitlab url to be https instead of http

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -1,7 +1,7 @@
 class profile::gitlab {
 
   class { 'gitlab':
-    external_url => hiera( 'gms_server_url', "http://${::fqdn}") ,
+    external_url => hiera( 'gms_server_url', "https://${::fqdn}") ,
   }
 
 }

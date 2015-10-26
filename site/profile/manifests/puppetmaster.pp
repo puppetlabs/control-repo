92f8d2d251d9e6fde5c124cb9a6974b1455269f7
Nick Walker <nick.walker@puppetlabs.com>
2015-10-26 14:59:20 -0700

    Configure the MoM to update the classifier after deploying code
    This entailed configring the classifier to never sync on a
    schedule.
    
    Changing environment_timeout to unlimited for all masters.
    
    Setting a postrun command for r10k that would update the class
    information in the classifier (the update-classes endpoint).

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -48,4 +48,20 @@ class profile::puppetmaster {
   }
   #END - Add deploy key and webhook to git management system
 
+  #Lay down update-classes.sh for use in r10k postrun_command
+  #This is configured via the pe_r10k::postrun key in hiera
+  file { '/usr/local/bin/update-classes.sh' :
+    ensure => file,
+    source => 'puppet::///modules/profile/puppetmaster/update-classes.sh',
+  }
+
+  #https://docs.puppetlabs.com/puppet/latest/reference/config_file_environment.html#environmenttimeout
+  ini_setting { "environment_timeout = unlimited":
+    ensure  => present,
+    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    section => 'main',
+    setting => 'environment_timeout',
+    value   => 'unlimited',
+  }
+
 }

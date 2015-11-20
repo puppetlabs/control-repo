f765903e25e2b7720a3cf52b78700275eb0197d5
Ranjit Viswakumar <whatsaranjit@gmail.com>
2015-11-20 13:29:17 +0000

    Checking for git first

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -3,5 +3,7 @@ if [ -e /opt/puppetlabs/server/pe_version ]
 then 
   /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2  
 else
-  /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD
+  /usr/bin/git --version > /dev/null 2>&1 &&
+  /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD ||
+  date +%s
 fi 

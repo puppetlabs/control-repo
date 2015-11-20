c2bd0d3ca1fd4e9f2fce24196201835272fd1e21
Ranjit Viswakumar <whatsaranjit@gmail.com>
2015-11-20 14:12:47 +0000

    Check for rugged or output UNIX epoch

--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,17 +1,23 @@
-require 'rugged'
+begin
+  require 'rugged'
+rescue LoadError => e
+  t = Time.new
+  puts t.to_i
+else
 
-environmentpath = ARGV[0]
-environment     = ARGV[1]
+  environmentpath = ARGV[0]
+  environment     = ARGV[1]
 
-repo = Rugged::Repository.discover(File.join(environmentpath, environment))
-head  = repo.head
+  repo = Rugged::Repository.discover(File.join(environmentpath, environment))
+  head  = repo.head
 
-#sha1 hash of the newest commit
-head_sha = head.target_id
+  #sha1 hash of the newest commit
+  head_sha = head.target_id
 
-#the commit message associated the newest commit
-commit = repo.lookup(head_sha)
+  #the commit message associated the newest commit
+  commit = repo.lookup(head_sha)
 
-#add something to find the remote url
+  #add something to find the remote url
 
-puts head_sha 
+  puts head_sha 
+end

a07a349baec6ed1f837d116250d0c9af829b7eb1
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-12-10 13:16:20 -0800

    Add os-specific sub-classes and have profile::base use case $::kernel

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,14 @@
+# Base profiles include all the classes that should be included on all nodes.
+# For cross-platform compatibility, this is broken up into platform-appropriate
+# subclasses.  This class applies the appropriate base profile based on the
+# $::kernel fact.
+
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
+  # Include a platform-appropriate base profile
+  case $::kernel {
+    'Linux':   { include profile::base::linux   }
+    'windows': { include profile::base::windows }
+  }
 
 }

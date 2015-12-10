d3a24a4d321380afce16fadaca687be27fb162a9
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-12-09 17:47:50 -0800

    Switch to cross-platform profile::base variant

--- site/role/manifests/database_server.pp
+++ site/role/manifests/database_server.pp
@@ -2,6 +2,6 @@ class role::database_server {
 
   #This role would be made of all the profiles that need to be included to make a database server work
   #All roles should include the base profile
-  include profile::base
+  include profile::base::linux
 
 }
--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -2,6 +2,6 @@ class role::webserver {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
-  include profile::base
+  include profile::base::linux
 
 }

ae6c530ff7b91338b9d59e51a2ac778aaaee2771
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-15 10:49:12 +1000

    Added monitoring

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,13 @@
 class profile::base {
 
   accounts::user { 'joe': }
+  
+  accounts::user { 'monitoring':
+    ensure     => present,
+    uid        => 450,
+    gid        => 450,
+    managehome => false,
+    locked     => true,
+    shell      => '/bin/false'
+  }
 }

5c8d747c203676895f4ddf9eaea0980bc93fde5b
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-15 10:50:10 +1000

    Changed to string

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,11 +1,11 @@
 class profile::base {
 
   accounts::user { 'joe': }
-  
+
   accounts::user { 'monitoring':
     ensure     => present,
-    uid        => 450,
-    gid        => 450,
+    uid        => '450',
+    gid        => '450',
     managehome => false,
     locked     => true,
     shell      => '/bin/false'

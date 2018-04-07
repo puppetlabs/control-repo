415a71dd59c17093684efd3a1376afe1bfb8824d
Romain Tartière <romain@blogreen.org>
2018-04-07 11:59:18 +0200

    Remove code dead for more than 2 years
    r10k generates a .r10k-deploy.json file since version 2.1.0 which was
    released on October 28, 2015.  New users of the control-repo are not
    likely to have a so old version of r10k, so remove this dead code.

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -6,14 +6,9 @@ if [ $# -ne 2 -o ! -d "$1" -o ! -d "$1/$2" ]; then
 fi
 
 ruby=ruby
-script="$1/$2/scripts/config_version.rb"
 
 if [ -x /opt/puppetlabs/puppet/bin/ruby ]; then
   ruby=/opt/puppetlabs/puppet/bin/ruby
 fi
 
-if [ -e $1/$2/.r10k-deploy.json ]; then
-  script="$1/$2/scripts/code_manager_config_version.rb"
-fi
-
-"${ruby}" "${script}" "$1" "$2"
+"${ruby}" "$1/$2/scripts/code_manager_config_version.rb" "$1" "$2"

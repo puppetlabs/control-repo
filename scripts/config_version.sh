211ecc58b5b650ed158f0c36158a85a289ce2640
Romain Tartière <romain@blogreen.org>
2018-04-07 11:34:07 +0200

    Do sanity checking early
    Fail before doing anything when passed parameters are invalid.

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $# -ne 2 -o ! -d "$1" -o ! -d "$1/$2" ]; then
+  echo "usage: $0 <environmentpath> <environment>" >&2
+  exit 1
+fi
+
 ruby=ruby
 script="$1/$2/scripts/config_version.rb"
 

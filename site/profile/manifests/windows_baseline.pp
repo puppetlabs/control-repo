f99ca0ab41ff622c26763f551d2085cae4c8e25e
Abir Majumdar <beersy@gmail.com>
2018-09-10 11:39:46 -0700

    Update windows_baseline.pp

--- site/profile/manifests/windows_baseline.pp
+++ site/profile/manifests/windows_baseline.pp
@@ -2,15 +2,46 @@
 class profile::windows_baseline {
 
   include chocolatey
-
-  package { 'unzip':
+  # PACKAGES
+  Package {
     ensure   => installed,
     provider => chocolatey,
   }
 
-  package { 'git':
-    ensure   => installed,
-    provider => chocolatey,
+  package { 'unzip': }
+  package { 'Firefox': }
+  package { 'git': }
+
+  # FIREWALL
+  windows_firewall::exception { 'TSErule':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'Allow',
+    enabled      => 'yes',
+    protocol     => 'TCP',
+    local_port   => '8080',
+    display_name => 'TSE PUPPET DEMO',
+    description  => 'Inbound rule example for demo purposes',
+  }
+  
+  # USERS
+  user { 'Puppet Demo':
+    ensure   => present,
+    groups   => ['Administrators'],
+  }
+  
+  # REG KEYS
+  registry_key { 'HKEY_LOCAL_MACHINE\Software\Demonstration':
+    ensure       => present,
+    purge_values => true,
+  }
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value1':
+    type => string,
+    data => 'this is a value new from puppet intro',
+  }
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value2':
+    type         => dword,
+    data         => '0xFFFFFFFF',
   }
 
 }

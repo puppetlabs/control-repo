141ffa19202bea2992588a10f45cff52cb396c95
Abir Majumdar <beersy@gmail.com>
2018-09-21 10:30:12 -0700

    Update linux_baseline.pp

--- site/profile/manifests/linux_baseline.pp
+++ site/profile/manifests/linux_baseline.pp
@@ -1,6 +1,8 @@
 # @summary This profile installs unzip and git as part of the Linux baseline
 class profile::linux_baseline {
 
+  include ntp
+  
   package { 'unzip':
     ensure => installed,
   }

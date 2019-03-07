2090fd085147c876c9055f0deffbc3d223797247
Abir Majumdar <beersy@gmail.com>
2019-03-07 17:24:38 -0500

    Update windows_webserver.pp

--- site/role/manifests/windows_webserver.pp
+++ site/role/manifests/windows_webserver.pp
@@ -1,7 +1,6 @@
 # @summary This role installs IIS and sample content on port 80.
 class role::windows_webserver {
   include profile::windows_baseline
-  include profile::vro_provisioned
   include profile::iis
   include profile::sample_website
 }

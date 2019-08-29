8ca3c28c954c9df14a09d9f676537e09a625e371
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-08-29 10:50:18 +0800

    atstst

--- site-modules/role/manifests/loadbalancer.pp
+++ site-modules/role/manifests/loadbalancer.pp
@@ -1,34 +1,33 @@
 class role::loadbalancer (
-  String   $ports1 = '80',
+  Optional[String]   $ports1 = '80',
   Optional[String]   $ports2 = undef,
-  String    $rule1 = 'http',
+  Optional[String]    $rule1 = 'http',
   Optional[String]    $rule2 = undef,
-  String   $backendserver_name1 = '',
-  String   $backendserver_name2 = '',
-  Optional[String]   $backendserver_ipaddress1 = undef,
-  Optional[String]   $backendserver_ipaddress2 = undef,
+  # String   $backendserver_name1 = '',
+  # String   $backendserver_name2 = '',
+  # Optional[String]   $backendserver_ipaddress1 = undef,
+  # Optional[String]   $backendserver_ipaddress2 = undef,
   ) {
   include ::haproxy
-  haproxy::listen { $rule1 :
+  haproxy::listen { 'puppet00' :
     collect_exported => false,
     ipaddress        => $::ipaddress,
-    ports            => $ports1,
+    ports            => '8140',
   }
 
-  haproxy::balancermember { $backendserver_name1 :
-    listening_service => 'puppetserver',
-    server_names      => $backendserver_name1,
-    ipaddresses       => $backendserver_ipaddress1,
-    ports             => $ports1,
+  haproxy::balancermember { 'haproxy' :
+    listening_service => 'puppet00',
+    server_names      => ['compilemaster-01.platform9.puppet.net','compilemaster-02.platform9.puppet.net'],
+    ipaddresses       => ['192.168.0.8','192.168.0.10'],
+    ports             => '8140',
     options           => 'check',
   }
-
-  haproxy::balancermember { $backendserver_name2 :
-    listening_service => 'puppetserver',
-    server_names      => $backendserver_name2,
-    ipaddresses       => $backendserver_ipaddress2,
-    ports             => $ports1,
-    options           => 'check',
-  }
+#  haproxy::balancermember { $backendserver_name2 :
+#    listening_service => 'puppetserver',
+#   server_names      => $backendserver_name2,
+#    ipaddresses       => $backendserver_ipaddress2,
+#    ports             => $ports1,
+#    options           => 'check',
+#  }
 
 }

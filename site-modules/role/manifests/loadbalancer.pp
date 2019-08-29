af58e06b792c8325b7cfc99f47816bb6174a9e37
Henry Wang <henry@Henrys-MacBook-Pro.local>
2019-08-29 21:04:13 +0800

    test class

--- site-modules/role/manifests/loadbalancer.pp
+++ site-modules/role/manifests/loadbalancer.pp
@@ -1,27 +1,26 @@
 class role::loadbalancer (
   Optional[String]   $ports1 = '80',
   Optional[String]   $ports2 = undef,
-  Optional[String]    $rule1 = 'puppet00',
-  Optional[String]    $rule2 = undef,
+  Optional[String]   $rule1 = 'puppet00',
+  Optional[String]   $rule2 = undef,
   Optional[String]   $backendserver_name1 = '',
   Optional[String]   $backendserver_name2 = '',
   Optional[String]   $backendserver_ipaddress1 = undef,
   Optional[String]   $backendserver_ipaddress2 = undef,
   ) {
-  include ::haproxy
-  haproxy::listen { $rule1 :
+
+  # include ::haproxy
+  #haproxy::listen { $rule1 :
+  class { 'haproxy::listen' :
     collect_exported => false,
     ipaddress        => $::ipaddress,
     ports            => $ports1,
   }
 
-  haproxy::balancermember { 'haproxy01' :
-    listening_service => $rule1,
-    # server_names      => ['compilemaster-01.platform9.puppet.net','compilemaster-02.platform9.puppet.net'],
-    # ipaddresses       => ['192.168.0.8','192.168.0.10'],
-    # ports             => '8140',
-    # options           => 'check',
 
+  #haproxy::balancermember { 'member1' :
+  class { 'haproxy::balancermember' :
+    listening_service => $rule1,
     server_names      => $backendserver_name1,
     ipaddresses       => $backendserver_ipaddress1,
     ports             => $ports1,
@@ -29,13 +28,18 @@ class role::loadbalancer (
 
   }
 
-  haproxy::balancermember { 'haproxy02' :
-
+  #haproxy::balancermember { 'haproxy02' :
+  class { 'haproxy::balancermember' :
    listening_service => $rule1,
    server_names      => $backendserver_name2,
    ipaddresses       => $backendserver_ipaddress2,
    ports             => $ports1,
    options           => 'check',
   }
+
+#pending Improvement, possible areas: Array Input with multipal ports
+
+
+
 
 }

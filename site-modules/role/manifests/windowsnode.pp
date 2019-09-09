979904ec5bd9d058237469686e9f9d3b07ccb323
Henry Wang <henry@henrys-mbp.wifi.bfs.corp.puppet.net>
2019-09-09 12:57:31 +0100

    d

--- site-modules/role/manifests/windowsnode.pp
+++ site-modules/role/manifests/windowsnode.pp
@@ -1,14 +1,15 @@
 class role::windowsnode {
 ###
   group { 'testgroup' :
-    name => "testgroup111",
+    name => 'testgroup111',
     ensure => present,
   }
 
 
   user { 'testuser1' :
-    name => "testuser111",
+    name => 'testuser111',
     ensure => present,
+    groups => 'testgroup111',
   }
 
 ###

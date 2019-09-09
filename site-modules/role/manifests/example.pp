b21c5e581650a43da580415a48c14584daf695fd
Henry Wang <henry@henrys-mbp.wifi.bfs.corp.puppet.net>
2019-09-09 12:50:02 +0100

    s

--- site-modules/role/manifests/example.pp
+++ site-modules/role/manifests/example.pp
@@ -1,11 +1,13 @@
 class role::example {
 
   group { 'testgroup' :
+    name   => ""testgroup111.
     ensure => present,
   }
 
 
   user { 'testuser1' :
+    name   => "testuser111",
     ensure => present,
   }
 

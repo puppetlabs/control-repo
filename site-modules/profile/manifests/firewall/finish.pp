22253d5888febbb0b8f2aa424a848ea29949767b
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-10-25 13:39:33 +0800

    tst

--- site-modules/profile/manifests/firewall/finish.pp
+++ site-modules/profile/manifests/firewall/finish.pp
@@ -7,15 +7,15 @@ class profile::firewall::finish {
 
   ['INPUT','OUTPUT'].each | $chain | {
 
-    # Drop the known noise from hitting the log
-    # ['255.255.255.255',ip_address(ip_broadcast("${::network}/${::netmask}"))].each | $dest | {
-    #   firewall { "990 Broadcasts for $dest for ${chain}":
-    #     destination  => $dest,
-    #     proto  => 'all',
-    #     action => 'drop',
-    #     chain  => $chain,
-    #   }
-    # } 
+    #Drop the known noise from hitting the log
+    ['255.255.255.255',ip_address(ip_broadcast("${::network}/${::netmask}"))].each | $dest | {
+      firewall { "990 Broadcasts for $dest for ${chain}":
+        destination  => $dest,
+        proto  => 'all',
+        action => 'drop',
+        chain  => $chain,
+      }
+    }
 
     # Log whatever hasn't been dealt with already
     firewall { "998 Logging for ${chain}":

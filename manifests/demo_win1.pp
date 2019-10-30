0e491d70bac1bd8547fe61cec06df470dae9bcd1
tonyjuric <54258912+tonyjuric@users.noreply.github.com>
2019-10-31 10:21:58 +1100

    Update demo_win1.pp

--- manifests/demo_win1.pp
+++ manifests/demo_win1.pp
@@ -1,13 +1,12 @@
-group { 'demo group':
-  name => 'test2',
-  ensure => present,
-  members => ['ajuric1'],
-  auth_membership => false,
-}
-
 user { 'ajuric1':
   ensure => present,
   password => 'Pass!3w0rd',
   groups => 'test2',
   auth_membership => inclusive
+}
+group { 'demo group':
+  name => 'test2',
+  ensure => present,
+  members => ['ajuric1'],
+  auth_membership => false,
 }

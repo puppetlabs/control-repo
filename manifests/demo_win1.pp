33f849d48d54aca6f8f9765adfd60a7105785538
tonyjuric <54258912+tonyjuric@users.noreply.github.com>
2019-11-07 10:41:48 +1100

    Update demo_win1.pp

--- manifests/demo_win1.pp
+++ manifests/demo_win1.pp
@@ -1,15 +1,17 @@
-user { 'ajuric':
-  ensure => present,
-  password => 'Pass!3w0rd',
-  groups => 'test2',
-  auth_membership => inclusive
-}
-group { 'demo group':
-  name => 'test2',
-  ensure => present,
-  auth_membership => false,
-}
-local_security_policy { 'Log on as a service':
-  ensure => present,
-  policy_value => 'cloudbase-init,NT_SERVICE\ALL_SERVICES,ajuric',
+if $::kernel == 'windows' {
+  user { 'ajuric':
+    ensure => present,
+    password => 'Pass!3w0rd',
+    groups => 'test2',
+    auth_membership => inclusive
+  }
+  group { 'demo group':
+    name => 'test2',
+    ensure => present,
+    auth_membership => false,
+  }
+  local_security_policy { 'Log on as a service':
+    ensure => present,
+    policy_value => 'cloudbase-init,NT_SERVICE\ALL_SERVICES,ajuric',
+  }
 }

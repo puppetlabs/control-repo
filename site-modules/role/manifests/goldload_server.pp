1fc40546959b3ea17af8ad4006b30f988885e94e
Henry Wang <henry@Henrys-MacBook-Pro.local>
2019-11-20 14:56:31 +0800

    s

--- site-modules/role/manifests/goldload_server.pp
+++ site-modules/role/manifests/goldload_server.pp
@@ -2,5 +2,5 @@ class role::goldload_server{
   include profile::base
 #  include profile::base::ciphers
   include profile::goldload::config
-  Class['profile::goldload::config'] -> Class['profile::base']
+#  Class['profile::goldload::config'] -> Class['profile::base']
 }

180cc2c06e6fa8e341e349b6c389296c313be86e
Henry Wang <henry@Henrys-MacBook-Pro.local>
2019-11-20 15:00:12 +0800

    ststst

--- site-modules/profile/manifests/goldload/software.pp
+++ site-modules/profile/manifests/goldload/software.pp
@@ -1,8 +1,8 @@
 class profile::goldload::software {
 
-  include profile::goldload::testlayer
 
   notify {'This is from profile::goldload::software': }
 
+  include profile::goldload::testlayer
 
 }

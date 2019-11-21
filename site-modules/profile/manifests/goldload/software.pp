aab8110c9d55b6af7a2ec70f7f5b9bfed111ec48
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-11-21 11:01:03 +0800

    tst

--- site-modules/profile/manifests/goldload/software.pp
+++ site-modules/profile/manifests/goldload/software.pp
@@ -3,6 +3,6 @@ class profile::goldload::software {
 
   notify {'This is from profile::goldload::software': }
 
-  contain profile::goldload::chocolatey
+  include profile::goldload::chocolatey
 
 }

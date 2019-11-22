cc7a353d57f874938cb42a1f0438c3c08af300a3
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-11-22 14:30:58 +0800

    tsts

--- site-modules/profile/manifests/toml-rbed.pp
+++ site-modules/profile/manifests/toml-rbed.pp
@@ -1,9 +1,9 @@
-class profile::toml-lb {
+class profile::toml-rbed {
   package {'toml-rb':
     ensure => present,
     provider => 'puppetserver_gem',
     notify => Service['pe-puppetserver']
-  } 
+  }
 
 
 }

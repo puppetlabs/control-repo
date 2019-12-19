25f98cbfd97f0cdfb99dad8b9fdd5d3129aee2fd
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-12-19 14:00:19 +0800

    sttt

--- site-modules/controlm/manifests/controlm_agent/install.pp
+++ site-modules/controlm/manifests/controlm_agent/install.pp
@@ -7,9 +7,9 @@ class controlm::controlm_agent::install inherits controlm::controlm_agent {
   context => '/files/etc/services',
   changes => [
     "ins service-name after service-name[last()]",
-    "set service-name[last()] ctmagent"
-    "set service-name [. = 'ctmagent']/port ${s2a_port} ",
-    "set service-name [. = 'ctmagent']/protocol tcp ",
+    "set service-name[last()] ctmagent",
+    "set service-name[. = 'ctmagent']/port ${s2a_port}",
+    "set service-name[. = 'ctmagent']/protocol tcp",
   #  "set service-name[port = '${s2a_port}'][protocol = 'udp']/ #comment 'Henry123']",
   #  "set service-name[port = '${s2a_port}'][protocol = 'tcp'] ctmagent",
   #  "set service-name[port = '${s2a_port}'][protocol = 'tcp']/ #comment 'Henry234']",

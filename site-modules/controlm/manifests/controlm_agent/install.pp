f263d59e08bb1c9fdcb7e309ab2a0cc843ad6af1
Henry Wang <henry@henrys-mbp.wifi.sin.corp.puppet.net>
2019-12-19 12:36:24 +0800

    sss

--- site-modules/controlm/manifests/controlm_agent/install.pp
+++ site-modules/controlm/manifests/controlm_agent/install.pp
@@ -7,18 +7,18 @@ class controlm::controlm_agent::install inherits controlm::controlm_agent {
   context => '/files/etc/services',
   changes => [
     "set service-name[port = '${s2a_port}'][protocol = 'udp'] ctmagent",
-    "set service-name[port = '${s2a_port}'][protocol = 'udp']/#comment 'Control-M server2agent'",
+#    "set service-name[port = '${s2a_port}'][protocol = 'udp'][#comment 'Control-M server2agent']",
     "set service-name[port = '${s2a_port}'][protocol = 'tcp'] ctmagent",
-    "set service-name[port = '${s2a_port}'][protocol = 'tcp']/#comment 'Control-M server2age  nt'",
+#    "set service-name[port = '${s2a_port}'][protocol = 'tcp'][#comment 'Control-M server2agent']",
     "set service-name[port = '${a2s_port}'][protocol = 'udp'] ctmagent",
-    "set service-name[port = '${a2s_port}'][protocol = 'udp']/#comment 'Control-M agent2servr'",
+#    "set service-name[port = '${a2s_port}'][protocol = 'udp']/ #comment 'Control-M agent2servr'",
     "set service-name[port = '${a2s_port}'][protocol = 'tcp'] ctmagent",
-    "set service-name[port = '${a2s_port}'][protocol = 'tcp']/#comment 'Control-M agent2server'",
+#    "set service-name[port = '${a2s_port}'][protocol = 'tcp']/ #comment 'Control-M agent2server'",
     ],
   }
 
 
-  # use shellscript and sudo as running the install direct as user ctmagent gives HOME errors plus we get errors in the log file 
+  # use shellscript and sudo as running the install direct as user ctmagent gives HOME errors plus we get errors in the log file
   # also does patches as can't get the patch script running after it for some reason
   # patch names need to be in hiera see controlm_agent.pp
   exec { 'controlm_agent_install':
@@ -51,9 +51,8 @@ class controlm::controlm_agent::install inherits controlm::controlm_agent {
               status    => '/bin/ps -ef | /bin/grep /apps/bmc/ctm/exe/p_ctm | /bin/grep -v grep',
               require   => [File['/etc/rc.d/init.d/ctm'],Exec['controlm_agent_install']]
             }
-            
+
       }
       default: {fail "Unsupported Major version ${::os['release']['major']} detected"}
   }
 }
-

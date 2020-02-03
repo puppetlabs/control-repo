aa4fbfb0071aa866635542aeec6631b1f6653b8a
Henry Wang <henry@Henrys-MacBook-Pro.local>
2020-02-03 12:49:55 +0800

    ss

--- site-modules/sqlserverhenry/manifests/win_sqlserver.pp
+++ site-modules/sqlserverhenry/manifests/win_sqlserver.pp
@@ -1,21 +1,10 @@
 class sqlserverhenry::win_sqlserver {
 
- sqlserver_instance{ 'MSSQLSERVER':
-  source                  => 'C:/',
-  features                => ['SQL'],
-  security_mode           => 'SQL',
-  sa_pwd                  => 'p@ssw0rd!!',
-  sql_sysadmin_accounts   => ['administrator'],
-  install_switches        => {
-    'TCPENABLED'          => 1,
-    'SQLBACKUPDIR'        => 'C:\\MSSQLSERVER\\backupdir',
-    'SQLTEMPDBDIR'        => 'C:\\MSSQLSERVER\\tempdbdir',
-    'INSTALLSQLDATADIR'   => 'C:\\MSSQLSERVER\\datadir',
-    'INSTANCEDIR'         => 'C:\\Program Files\\Microsoft SQL Server',
-    'INSTALLSHAREDDIR'    => 'C:\\Program Files\\Microsoft SQL Server',
-    'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server',
-  }
-}
+  sqlserver_instance{ 'MSSQLSERVER':
+     features                => ['SQL'],
+     source                  => 'E:/',
+     sql_sysadmin_accounts   => ['myuser'],
+ }
 
     sqlserver::sp_configure{'Turn On Advanced':
     config_name => 'show advanced option',

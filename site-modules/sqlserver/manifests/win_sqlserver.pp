99bd0521315e2bfc99e74f95723b65866de6024d
Henry Wang <henry@Henrys-MacBook-Pro.local>
2020-02-03 12:39:09 +0800

    dd

--- site-modules/sqlserver/manifests/win_sqlserver.pp
+++ site-modules/sqlserver/manifests/win_sqlserver.pp
@@ -1,5 +1,6 @@
 class sqlserver::win_sqlserver {
-  sqlserver_instance{ 'MSSQLSERVER':
+
+ ::sqlserver_instance{ 'MSSQLSERVER':
   source                  => 'C:/',
   features                => ['SQL'],
   security_mode           => 'SQL',
@@ -15,5 +16,11 @@ class sqlserver::win_sqlserver {
     'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server',
   }
 }
+
+    ::sqlserver::sp_configure{'Turn On Advanced':
+    config_name => 'show advanced option',
+    value       => 1,
+    reconfigure => true,
+  }
 
 }

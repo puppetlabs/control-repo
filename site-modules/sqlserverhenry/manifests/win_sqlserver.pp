0d8393f906a774fe3447f0b54b2614932f77a178
Henry Wang <henry@Henrys-MacBook-Pro.local>
2020-02-03 13:02:45 +0800

    sfsd

--- site-modules/sqlserverhenry/manifests/win_sqlserver.pp
+++ site-modules/sqlserverhenry/manifests/win_sqlserver.pp
@@ -1,19 +1,8 @@
 class sqlserverhenry::win_sqlserver {
   sqlserver_instance{ 'MSSQLSERVER':
-  source                  => 'C:/',
-  features                => ['SQL'],
-  security_mode           => 'SQL',
-  sa_pwd                  => 'p@ssw0rd!!',
-  sql_sysadmin_accounts   => ['myuser'],
-  install_switches        => {
-    'TCPENABLED'          => 1,
-    'SQLBACKUPDIR'        => 'C:\\MSSQLSERVER\\backupdir',
-    'SQLTEMPDBDIR'        => 'C:\\MSSQLSERVER\\tempdbdir',
-    'INSTALLSQLDATADIR'   => 'C:\\MSSQLSERVER\\datadir',
-    'INSTANCEDIR'         => 'C:\\Program Files\\Microsoft SQL Server',
-    'INSTALLSHAREDDIR'    => 'C:\\Program Files\\Microsoft SQL Server',
-    'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server',
+      features                => ['SQL'],
+      source                  => 'C:/SQLEXPR_x64_ENU.exe',
+      sql_sysadmin_accounts   => ['myuser'],
   }
-}
 
 }

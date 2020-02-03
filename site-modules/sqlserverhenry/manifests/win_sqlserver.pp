0759e57bff7844bc3c24a44d2e6e3a8ce9b256cd
Henry Wang <henry@Henrys-MacBook-Pro.local>
2020-02-03 13:05:39 +0800

    sfdsf

--- site-modules/sqlserverhenry/manifests/win_sqlserver.pp
+++ site-modules/sqlserverhenry/manifests/win_sqlserver.pp
@@ -1,7 +1,7 @@
 class sqlserverhenry::win_sqlserver {
   sqlserver_instance{ 'MSSQLSERVER':
       features                => ['SQL'],
-      source                  => 'C:/SQLEXPR_x64_ENU.exe',
+      source                  => 'C:\\SQLEXPR_x64_ENU',
       sql_sysadmin_accounts   => ['myuser'],
   }
 

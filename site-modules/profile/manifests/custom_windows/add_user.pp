0e53e2d15c9dce741ce5c1514994a84b51755f52
christopher.lawrence <chris.lawrence@puppet.com>
2020-02-07 12:29:53 +0000

    managing directories with ACLs

--- site-modules/profile/manifests/custom_windows/add_user.pp
+++ site-modules/profile/manifests/custom_windows/add_user.pp
@@ -11,4 +11,33 @@ class profile::custom_windows::add_user {
     manage_home   => true,
     user_file     => 'myfile',
   }
+
+    acl { 'C:\\Users\\myuser\myfile':
+      permissions => [
+      { identity => myuser, rights => ['read','execute'] },
+      { identity => mygroup, rights => ['read'] }
+     ],
+    }
+
+    registry::value { 'Enable IE ESC':
+      key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
+      value => 'IsInstalled',
+      data  => '1',
+      type  => 'dword',
+    }  
+
+
+    registry::value { 'Enable shutdown tracker':
+      key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability',
+      value => 'ShutdownReasonOn',
+      data  => '1',
+      type  => 'dword',
+    }
+
+   registry::value { 'Enable shutdown tracker UI':
+     key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability',
+     value => 'ShutdownReasonUI',
+     data  => '1',
+     type  => 'dword',
+   }
 }

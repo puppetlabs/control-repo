5985bd1450c0431dfd7a5ba5569d5367b459b7d9
christopher.lawrence <chris.lawrence@puppet.com>
2020-03-06 10:29:43 +0000

    setting up the wonderful cisbench and facter cache modules for securing my platforms in a lovely way

--- site-modules/profile/manifests/puppetserver/authority.pp
+++ site-modules/profile/manifests/puppetserver/authority.pp
@@ -2,7 +2,7 @@ class profile::puppetserver::authority (
 
   String $jwt_secret = 'undef',
   String $loglevel   = 'info',
-  Integer $validity  = '0',
+  String $validity   = '0',
   String $ensure     = 'latest',
   Hash $config       = {},
   Hash $jwt_token    = {},
@@ -22,7 +22,7 @@ class profile::puppetserver::authority (
         loglevel => $loglevel,
       },
       jwt_token => {
-        secret   => \"$jwt_secret\",
+        secret   => $jwt_secret,
         validity => $validity,
       }
     },

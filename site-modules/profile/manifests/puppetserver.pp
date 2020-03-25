d8f5a0d673476c9d68408b9df192a5524f500faa
christopher.lawrence <chris.lawrence@puppet.com>
2020-03-25 20:21:59 +0000

    adding in metrics collector and packages

--- site-modules/profile/manifests/puppetserver.pp
+++ site-modules/profile/manifests/puppetserver.pp
@@ -1,11 +1,17 @@
 class profile::puppetserver (
-  Boolean $puppetserver = true,
-  Boolean $authority    = false,
+  Boolean $puppetserver      = true,
+  Boolean $authority         = false,
+  Boolean $metrics_server    = true,
 ) {
   if $puppetserver {
     class { '::profile::puppetserver::bootstrap': }
   }
   if $authority {
     class { '::profile::puppetserver::authority': }
+  }
+  if $metrics_server {
+    include puppet_metrics_collector
+    include puppet_metrics_collector::system
+    include puppet_metrics_dashboard::profile::master::postgres_access
   }
 }

1cb0998061eec3d9d04a3d6246294e84ed3b9e07
Gloria Treider <asefehgj@ntnu.no>
2020-10-02 16:50:44 +0200

    sitepp kommer

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,3 +29,26 @@ node default {
   # Example:
   #   class { 'my_class': }
 }
+node default {
+ notify { "Oops Default! I'm ${facts['hostname']}": }
+}
+node 'win.node.consul' {
+ include ::profile::base_windows
+ include ::profile::dns::client
+}
+node /lin\d?.node.consul/ { 
+ include ::profile::base_linux 
+ include ::profile::dns::client 
+ include ::profile::consul::client
+}
+node /(manager|mon).node.consul/ {
+ include ::profile::base_linux
+ include ::profile::dns::client
+ include ::profile::consul::server
+}
+node 'dir.node.consul' {
+ include ::profile::base_linux
+ include ::profile::dns::client
+ include ::profile::dns::server
+ include ::profile::consul::server
+}

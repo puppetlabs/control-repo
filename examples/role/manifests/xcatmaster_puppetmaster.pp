2b90627a037cb6ba306bae0e360bd4baae477edb
Peter Enstrom <enstrom@illinois.edu>
2021-04-16 10:49:52 -0500

    uncomment puppet-profile_timesync in default role manifest to test it out

--- examples/role/manifests/xcatmaster_puppetmaster.pp
+++ examples/role/manifests/xcatmaster_puppetmaster.pp
@@ -6,7 +6,7 @@ class role::xcatmaster_puppetmaster {
   include ::profile::squid_proxy
   include ::profile::sssd
   include ::profile_allow_ssh_from_cerberus
-  include ::profile_chrony::server
+  include ::profile_timesync::server
   include ::profile_docker
   include ::profile_puppet_master
   include ::profile_xcat::master
--- site-modules/role/manifests/default.pp
+++ site-modules/role/manifests/default.pp
@@ -4,7 +4,7 @@ class role::default {
 
   include ::profile::base
   # include ::profile::rsyslog::client
-  # include ::profile_chrony::client
+  include ::profile_timesync::client
   # include ::profile_firewall
   # include ::profile_telegraf # depends on github.com/ncsa/puppet-profile_telegraf
 

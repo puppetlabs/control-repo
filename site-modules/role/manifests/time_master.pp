5b0d112b19ef3de6ed18c57e9d6b34df3fe2ce7f
Peter Enstrom <enstrom@illinois.edu>
2021-05-03 15:03:20 -0500

    Added new role time_master

---
+++ site-modules/role/manifests/time_master.pp
@@ -0,0 +1,11 @@
+# @summary Default role
+
+class role::default {
+
+  include ::profile::base
+  # include ::profile::rsyslog::client
+  include ::profile_timesync::server
+  # include ::profile_firewall
+  # include ::profile_telegraf # depends on github.com/ncsa/puppet-profile_telegraf
+
+}

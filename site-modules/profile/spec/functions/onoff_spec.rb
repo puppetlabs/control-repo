1235b5db0c33a55d5f2523618815ad31f6ea0bb8
Zach Bensley <zach.bensley@perforce.com>
2026-06-11 11:09:47 +0800

    feat: add example functions

---
+++ site-modules/profile/spec/functions/onoff_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'profile::onoff' do
+  it { is_expected.to run.with_params(true).and_return('On') }
+  it { is_expected.to run.with_params('true').and_return('On') }
+  it { is_expected.to run.with_params('TrUe').and_return('On') }
+
+  it { is_expected.to run.with_params(false).and_return('Off') }
+  it { is_expected.to run.with_params('false').and_return('Off') }
+  it { is_expected.to run.with_params('FaLsE').and_return('Off') }
+
+  it { is_expected.to run.with_params('standby').and_return('standby') }
+end
